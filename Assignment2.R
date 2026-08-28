26845af8c95a238a89ad8653fff02681c50dce81
Dylan Caudill <dc33@usf.edu>
2026-08-28 09:18:08 -0400

    Adding Assignment2

---
+++ Assignment2.R
@@ -0,0 +1,11 @@
+assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
+myMean <- function(assignment2) {
+  return(sum(assignment) / length(someData))
+}
+myMean(assignment2)
+# Variable names were wrong in the MYmean function
+# Updated Variables below 
+myMean <- function(assignment2) {
+  return(sum(assignment2) / length(assignment2))
+}
+myMean(assignment2)
